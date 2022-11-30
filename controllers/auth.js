a5699166b97d246884cf94f45ff5b0567fff15fb
aleksei.sushkevich <kakoriginalnenko@gmail.com>
2022-11-30 17:28:43 +0300

    added jsonwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 
-exports.signup = async (req, res, next) => {
+exports.signup = async (req, res ) => {
     const body = req.body;
     try {
         const hashedPw = await bcrypt.hash(body.password, 12);
@@ -20,21 +21,28 @@ exports.signup = async (req, res, next) => {
 
 };
 
-exports.login = async (req, res, next) => {
+exports.login = async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     let loadedUser;
     try {
         const user = await User.findOne({ email: email });
         if (!user) {
-            throw new Error('A user with this email could not be found.');
+            new Error('A user with this email could not be found.');
         }
         loadedUser = user;
         const isEqual = await bcrypt.compare(password, user.password);
         if (!isEqual) {
-            throw new Error('Wrong password!');
+            new Error('Wrong password!');
         }
-        res.status(200).json({userId: loadedUser._id.toString()});
+        const token = jwt.sign(
+            {
+                email: email,
+                userId: loadedUser._id.toString()
+            },
+            '21391249194914912934154152-09'
+        );
+        res.status(200).json({userId: loadedUser._id.toString(), token: token});
     } catch (err) {
         res.status(400).json({
             message: err.message
